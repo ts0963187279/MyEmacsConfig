9f9f75f3533d5968e0dbfa698bba68518d597884
ts0963187279 <ts0963187279@gmail.com>
2017-12-28 15:13:04 +0800

    add android-mode

--- init.el
+++ init.el
@@ -7,5 +7,19 @@
 (require 'init_auto-complete)
 (require 'init_direx)
 (require 'init_linum-relative)
+(require 'init_android-mode)
 ;;android-mode
-(require 'android-mode)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (android-mode linum-relative direx dired+ auto-complete))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
